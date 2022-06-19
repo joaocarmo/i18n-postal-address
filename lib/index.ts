8bba9ff1e7a32433b845f4f78052b3d1d581cbdf
João Carmo <17721880+joaocarmo@users.noreply.github.com>
2022-06-19 18:27:03 +0100

    [Feat] Expose inner types (#40)
    * Mark .raw() as deprecated
    
    * Expose the inner types through PostalAddressType
    
    * Upgrades a few dependencies

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,8 @@
 import PostalAddress from './postal-address'
+import type * as PostalAddressType from './types/address-format'
 
+// Export the PostalAddress main class
 export default PostalAddress
+
+// Export the inner types
+export type { PostalAddressType }
